c9d0f622396327bceb0595978f6366b8e3a061a8
MulletBoy42 <96805001+MulletBoy42@users.noreply.github.com>
2022-03-28 14:32:11 -0500

    Exercise 1 Day13 is done

---
+++ Day13/Property-binding-image-size/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  width:number=500
+  height:number=250
+  title:string= 'Property-binding-image-size';
+}
